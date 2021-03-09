2228cf89f134d8cd42eb6f4d3da382ebfab22b31
AREEG94FAHAD <fahedareeg@gmail.com>
2021-03-09 05:32:38 -0800

    Update resizeImage.py

--- resizeImage.py
+++ resizeImage.py
@@ -8,7 +8,6 @@ import os
 
 
 
-
 API_TOKEN = '1613485836:AAFSGMb5kMJqeA2X112-2lC01HeuIisimVg'
 
 bot = telebot.TeleBot(API_TOKEN)
@@ -27,7 +26,7 @@ def send_welcome(message):
     height.clear()
     msg = bot.reply_to(message, """\
     Hi these are photo size of almost social media
-💙Facebook💙
+Facebook
 profile picture size: 180 x 180
 cover photo size: 820 x 312 
 Story ad size: 1080 x 1920 
@@ -37,18 +36,18 @@ highlighted image size: 1200 x 717
 event image size: 1920 x 1005 
 group cover image size: 1640 x 856 
 image ad size: 1200 x 628 
-🧡Instagram🧡
+Instagram
 profile picture size: 110 x 110
 photo sizes: 1080 x 1080
 Stories size: 1080 x 1920
 IGTV Cover Photo Size: 420 x 654 
 Instagram Reels: 1080 x 1920  
-🧡 Twitter🧡  
+Twitter  
 profile picture size: 400 x 400
 header size: 1500 x 500
 post image size: 1200 x 675
 image size for ads: 800 x 418
-🤎LinkedIn🤎
+LinkedIn
 company logo size: 300 x 300
 cover photo size: 1128 x 191
 Dynamic Ads size: 100 x 100 
@@ -56,21 +55,21 @@ Stories image size: 1080 x 1920
 profile picture size: 400 x 400
 background photo size: 1584 x 396
 post image size: 1200 x 1200
-💗YouTube💗
+YouTube
 profile photo size: 800 x 800
 Channel cover picture: 2560 x 1440 (desktop) 
 Channel cover picture 1546 x 423 (smartphones)
 Display ads: 300 x 250
-💛Pinterest💛
+Pinterest
 Profile picture: 165 x 165
 Board Display image: 222 x 150
 Standard Pin size: Vertical images 1000 x 1500
 Story Pins image size: 1080 x 1920
-🤍Tik Tok🤍
+Tik Tok
 Profile photo: 200 x 200
 """)
 
-    msg = bot.reply_to(message, '🥰 please send me the width of the photo you want to resize.')
+    msg = bot.reply_to(message, ' please send me the width of the photo you want to resize.')
     bot.register_next_step_handler(msg, process_width_step)
 
 def process_width_step(message):
@@ -85,10 +84,10 @@ def process_width_step(message):
             return
 
         width.append(int(message.text))
-        msg = bot.reply_to(message, 'Please send me the height of the photo you want to resize 🥰')
+        msg = bot.reply_to(message, 'Please send me the height of the photo you want to resize ')
         bot.register_next_step_handler(msg, process_height_step)
     except Exception as e:
-        bot.reply_to(message, 'Something went wrong 🥺')
+        bot.reply_to(message, 'Something went wrong ')
 
 def process_height_step(message):
     try:
@@ -100,10 +99,10 @@ def process_height_step(message):
             bot.register_next_step_handler(msg, process_height_step)
             return
         height.append(int(message.text))
-        msg = bot.reply_to(message, 'Please send me the the photo you want to resize 🥰')
+        msg = bot.reply_to(message, 'Please send me the the photo you want to resize ')
         bot.register_next_step_handler(msg, process_send_file)
     except Exception as e:
-        bot.reply_to(message, 'Something went wrong 🥺')
+        bot.reply_to(message, 'Something went wrong ')
 
 
 def process_send_file(message):
@@ -114,21 +113,21 @@ def process_send_file(message):
         imageName=get_random_string(16)
         with open(imageName+".jpg", 'wb') as new_file:
             new_file.write(downloaded_file)
-        bot.reply_to(message, '10 second left 💜')
+        bot.reply_to(message, '10 second left ')
         image = Image.open(imageName+".jpg")
         # print(width)
         # print(height)
         new_image = image.resize((width[0], height[0]))
         new_image.save(imageName+".jpg")
-        bot.reply_to(message, '5 second left 💜')
+        bot.reply_to(message, '5 second left ')
         chat_id = message.chat.id
         doc = open(imageName+".jpg", 'rb')
         bot.send_document(chat_id, doc)
         doc.close()
-        msg = bot.reply_to(message, '❤️ Thank you for useing my bot. Areeg Fahad ❤️')
+        msg = bot.reply_to(message, ' Thank you for useing my bot. Areeg Fahad ')
         os.remove(imageName+".jpg")
     except Exception as e:
-        bot.reply_to(message, '😶 another time send photo ok 😶')
+        bot.reply_to(message, ' another time send photo ok')
 
 
 # def removeImage(message):
@@ -137,7 +136,6 @@ def process_send_file(message):
 #         bot.reply_to(message, 'thank you')
 #     except Exception as e:
 #         bot.reply_to(message, 'oooops')       
-
 
 
     
